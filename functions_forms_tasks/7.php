ddc83c876714bd460f887e8324a6addd131a9d62
AlionaDavydenko <petrechenko_@inbox.ru>
2016-02-12 18:36:00 +0200

    Update and rename 9.php to 7.php

---
+++ functions_forms_tasks/7.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<form method="get" action="9.php">
+<u><i>Пользователь:</i></u><br>
+<input id="name" name="Пользователь" placeholder ="Введите ваше имя"><br>
+<u><i>Комментарий:</i></u><br>
+<textarea input type="text" name="Комментарий" placeholder ="Введите ваш комментарий"></textarea><br>
+	<input type="submit" value = "Отправить"/><br><br>
+
+</form>
+</body>
+</html>
+<?php
+function comment($com){
+	$k = " ";
+	foreach($_GET as $key=>$val){
+		$k.= "{$val} <br>";
+	}
+	echo $k;
+}
+if($_GET){
+	comment($_GET);
+}else{
+		"Оставьте ваш комментарий";
+}
+?>
