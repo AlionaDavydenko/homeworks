1bed07cf7384b1b3020eab0f8ca8c55a4375b7c7
AlionaDavydenko <petrechenko_@inbox.ru>
2016-02-19 12:04:31 +0200

    Update 10.php

--- functions_forms_tasks/10.php
+++ functions_forms_tasks/10.php
@@ -1,14 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+    <body>
+<form action="10.php" method="post">
+<u>Ваше имя:</u><br>
+<input type="text" name="name" placeholder ="Ваше имя"/><br><br>
+<u>Комментарий:</u><br>
+<textarea input type="text" name="comment" placeholder ="Введите ваш комментарий"></textarea><br>
+	<input type="submit" value = "Отправить"/><br><br>
+</form>
+    </body>
+</html>
 <?php
 /*
 10. Написать функцию, которая считает количество уникальных слов в тексте. Слова разделяются пробелами. Текст должен вводиться с формы.
 */
-
 function uniqWords($text){
-	$words = explode(" ", $text);
+    $words = explode(" ", $text);
     $words = array_unique($words);
     return $words;
 }
     $text = $_POST['comment'];
-	$unique = uniqWords($text);
+    $unique = uniqWords($text);
 echo count($unique);
 	
